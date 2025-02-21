c57bdb9c59e71789d3c1640aa90cc1ad374a915c
wisangdrillian <gwisang45@gmail.com>
2025-02-21 22:34:05 +0700

    memperbaiki style nav

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,42 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/nav.css";
-import duitkuLogo from '../../assets/duitku-logo.png'
+import duitkuLogo from "../../assets/duitku-logo.png";
 
 const Navigation: React.FC = () => {
-    return (
-        <nav className="sidenav">
-            <div className="logo-container">
-                   <img src={duitkuLogo} alt="logo" height={100} width={100} />
-            </div>
-            <div className="nav-menu">
-                <Link to="/" className="nav-item active">
-                    <div className="nav-icon">
-                        <i className="fas fa-house"></i>
-                    </div>
-                    <span>Dashboard</span>
-                </Link>
-                <Link to="/anggaran" className="nav-item">
-                    <div className="nav-icon">
-                        <i className="fas fa-wallet"></i>
-                    </div>
-                    <span>Anggaran</span>
-                </Link>
-                <Link to="/investasi" className="nav-item">
-                    <div className="nav-icon">
-                        <i className="fas fa-chart-simple"></i>
-                    </div>
-                    <span>Investasi</span>
-                </Link>
-                <Link to="/tujuan" className="nav-item">
-                    <div className="nav-icon">
-                        <i className="fas fa-bullseye"></i>
-                    </div>
-                    <span>Tujuan</span>
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="nav-container">
+      <div className="logo-container">
+        <img src={duitkuLogo} alt="logo" className="logo" />
+      </div>
+      <div className="nav-menu">
+        <Link to="/" className="nav-item active">
+          <div className="nav-icon">
+            <i className="fas fa-house"></i>
+          </div>
+          <span>Dashboard</span>
+        </Link>
+        <Link to="/anggaran" className="nav-item">
+          <div className="nav-icon">
+            <i className="fas fa-wallet"></i>
+          </div>
+          <span>Anggaran</span>
+        </Link>
+        <Link to="/investasi" className="nav-item">
+          <div className="nav-icon">
+            <i className="fas fa-chart-simple"></i>
+          </div>
+          <span>Investasi</span>
+        </Link>
+        <Link to="/tujuan" className="nav-item">
+          <div className="nav-icon">
+            <i className="fas fa-bullseye"></i>
+          </div>
+          <span>Tujuan</span>
+        </Link>
+        <Link to="/account" className="nav-item">
+          <div className="nav-icon">
+            <i className="fas fa-user"></i>
+          </div>
+          <span>Akun</span>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
